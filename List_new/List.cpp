abac770f2ba80419052a5cba1229c85ce455275c
shilong.song <shilong.song@wxbester.cn>
2025-03-21 16:05:59 +0800

    1

--- List_new/List.cpp
+++ List_new/List.cpp
@@ -20,9 +20,85 @@ ListNode<T> *ListNode<T>::insertPre(const T &data)
     ListNode<T> *x = new ListNode(data, this->pre, this);
     this.pre->succ = x;
     this.pre = x;
+    return n;
+}
+// 有头尾指针插入节点方法一般化，不然空插入一个节点，
+// this指的是要创建的对象 new初始化也要相应修改
+// 还是要一个头记录，不然不行，new出来的新节点，前驱后继只能没有了
+template <typename T>
+ListNode<T> *ListNode<T>::insertSuc(const T &data)
+{
+    ListNode<T> n = new ListNode<T>(data, this, this->suc);
+    this->suc->pre = n;
+    this.suc = n;
+    return n;
+}
+
+template <typename T>
+ListNode<T> *List<T>::insertFront(const T &data)
+{
+    // ListNode<T> *ptr = new ListNode<T>(data, this->header, this->header->suc);
+    // this->header->suc->pre = ptr;
+    // this->header->suc = ptr;
+    _size++;
+    return header->insertSuc(data);
 }
 template <typename T>
-ListNode<T> *List<T>::insertPre(ListNode<T> *p, const T &data)
+ListNode<T> *List<T>::insertLast(const T &data)
 {
+    this->_size++;
+    return trailer->insertPre(data);
+}
+
+template <typename T>
+ListNode<T> *List<T>::insertPointPre(ListNode<T> *p, const T &data)
+{
+    this->_size++;
     return p->insertPre(data);
+}
+
+template <typename T>
+ListNode<T> *List<T>::insertPointSuc(ListNode<T> *p, const T &data)
+{
+    this->_size++;
+    return p->insertSuc(data);
+}
+
+template <typename T>
+ListNode<T> *List<T>::find(const T &data, ListNode<T> *lo, unsigned int num) const
+{
+    while (num > 0 && lo != nullptr)
+    {
+        if ((lo->data) != data)
+        {
+            // 此处链表传后继是不是有问题 lo变了原来实参怎么样了
+            lo = lo->suc;
+            num--;
+        }
+        else
+        {
+            return lo;
+        }
+    }
+    return nullptr;
+}
+
+template <typename T>
+List<T> List<T>::copyNodes(ListNode<T> *lo, unsigned int nums)
+{
+    List<T> lt = new List<T>();
+    while (nums > 0)
+    {
+        lt.insertLast(lo->data);
+        lo = lo->suc;
+    }
+}
+
+template <typename T>
+void List<T>::remove(ListNode<T> *p)
+{
+    p->pre->suc = p->suc;
+    p->suc->pre = p->pre;
+    this->_size--;
+    delete p;
 }
--- List_new/List.h
+++ List_new/List.h
@@ -34,6 +34,16 @@ public:
     // 返回创建的新节点
     ListNode<T> *insertFront(const T &data);
     ListNode<T> *insertLast(const T &data);
-    ListNode<T> *insertPre(ListNode<T> *p, const T &data);
-    ListNode<T> *insertSuc(ListNode<T> *p, const T &data);
+    ListNode<T> *insertPointPre(ListNode<T> *p, const T &data);
+    ListNode<T> *insertPointSuc(ListNode<T> *p, const T &data);
+    // find 在范围内查找lo开始的后num个节点
+    ListNode<T> *find(const T &data, ListNode<T> *lo, unsigned int num) const;
+    // copyNodes
+    List<T> copyNodes(ListNode<T> *lo, unsigned int nums);
+    // void remove(ListNode<T> *p) 删除指针指向，并销毁空间
+    void remove(ListNode<T> *p);
+    // 析构函数
+    // clear()删除每一个节点
+    // unique
+    // 借助函数指针机制遍历
 };
