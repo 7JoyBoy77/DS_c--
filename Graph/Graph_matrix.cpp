8bee237b73d6d77b2e0c5115e51f983de4d5d2d5
shilong.song <shilong.song@wxbester.cn>
2025-05-20 17:01:38 +0800

    1

--- Graph/Graph_matrix.cpp
+++ Graph/Graph_matrix.cpp
@@ -21,7 +21,11 @@ public:
     };
     // 析构如何析构
     ~GraphMatrix();
-    Tv &vertex(Rank i) override { return V[i].data; }
+
+    Tv &vertex(Rank i) override
+    {
+        return V[i].data;
+    }
     Rank inDegree(Rank i) override { return V[i].inDegree; }
     Rank outDegree(Rank i) override { return V[i].outDegree; }
     VStatus &status(Rank i) override { return V[i].status; }
@@ -34,9 +38,86 @@ public:
     // 顶点动态操作
     Tv remove(Rank i) override
     {
+        for (int j = 0; j < n; j++)
+        {
+            if (exists(i, j))
+            {
+                delete E[i][j];
+                V[j].inDegree--;
+            }
+        }
+        E.erase(i);
+        n--;
+        Tv vBak = vertex(i);
+        V.erase(i);
+        for (int j = 0; j < n; j++)
+        {
+            if (Edge<Te> *e = E[j].remove(i))
+            {
+                delete e;
+                V[j].outDegree--;
+            }
+        }
+        return vBak;
     }
+    /**
+     * @brief 并没有直接插入节点Vertex，传递一个Tv类型数据，用它构造一个Vertex对象
+     *
+     * @param v
+     * @return Rank
+     */
     Rank insert(const Tv &v) override
     {
+        // 为各顶点预留一条潜在边，这里是为什么
+        for (int i = 0; i < n; i++)
+        {
+            E[i].insert(nullptr);
+        }
+        n++;
+        E.insert(Vector<Edge<Te> *>(n, n, (Edge<Te> *)nullptr));
+        return V.insert(Vertex<Tv>(v));
+    }
+
+    Rank firstNbr(Rank i) override
+    {
+        return nextNbr(i, n);
+    }
+
+    Rank nextNbr(Rank i, Rank j) override
+    {
+        while (-1 < (j--) && !exists(i, j))
+        {
+        }
+        return j;
+    }
+    EType Te &edge(Rank i, Rank j) override
+    {
+        return E[i][j].data;
     }
     // 边的动态操作
+    void insert(const Te &e, int w, Rank i, Rank j) override
+    {
+        if (exists(i, j))
+        {
+            return;
+        }
+        E[i][j] = new Edge<Te>(e, w);
+        this->e++;
+        V[i].outDegree++;
+        V[j].inDegree++;
+    }
+    void remove(int i, int j) override
+    {
+        Te temp = edge(i, j);
+        if (!exists(i, j))
+        {
+            return;
+        }
+        delete E[i][j];
+        E[i][j] = nullptr;
+        this->e--;
+        V[i].outDegree--;
+        V[j].inDegree--;
+        return temp;
+    }
 };
--- Graph/graph.cpp
+++ Graph/graph.cpp
@@ -11,3 +11,22 @@ Edge<Te>::Edge(const Te &d, int w)
     : data(d), weight(w), type(UNDETERMINED)
 {
 }
+
+template <typename Tv, typename Te>
+void Graph<Tv, Te>::BFS(int v, int &clock)
+{
+    Queue<int> Q;
+    status(v) = DISCOVERED;
+    Q.enqueue(v);
+    while (!Q.empty())
+    {
+        int v = Q.dequeue();
+        dTime(v) = ++clock;
+        for (int u = firstNbr(); -1 < u; u = nextNbr(u, v))
+        {
+            if (status(u) = UNDISCOVERED)
+            {
+                        }
+        }
+    }
+}
